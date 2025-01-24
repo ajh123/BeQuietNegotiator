611706195664348707c24b7a45992431131bde59
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-01-24 19:58:53 +0000

    Fix ChannelAcceptor by removing even bus init

--- src/main/java/me/ajh123/channel_acceptor/ChannelAcceptor.java
+++ src/main/java/me/ajh123/channel_acceptor/ChannelAcceptor.java
@@ -4,7 +4,6 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.neoforge.common.NeoForge;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(ChannelAcceptor.MODID)
@@ -14,13 +13,7 @@ public class ChannelAcceptor
     public static final String MODID = "channel_acceptor";
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public ChannelAcceptor(IEventBus modEventBus, ModContainer modContainer)
-    {
-        // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (ChannelAcceptor) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
-        NeoForge.EVENT_BUS.register(this);
-
+    public ChannelAcceptor(IEventBus modEventBus, ModContainer modContainer) {
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
