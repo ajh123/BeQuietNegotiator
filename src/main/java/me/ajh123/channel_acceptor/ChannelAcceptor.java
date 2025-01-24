86f3f3019db619357118af2bb90dfa67d334c7db
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-01-24 17:47:56 +0000

    Refactor mod name and package from 'channel_acepptor' to 'channel_acceptor'; update related configurations and language files.

--- src/main/java/me/ajh123/channel_acceptor/ChannelAcceptor.java
+++ src/main/java/me/ajh123/channel_acceptor/ChannelAcceptor.java
@@ -1,4 +1,4 @@
-package me.ajh123.channel_acepptor;
+package me.ajh123.channel_acceptor;
 
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
@@ -7,17 +7,17 @@ import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.NeoForge;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(ChannelAcepptor.MODID)
-public class ChannelAcepptor
+@Mod(ChannelAcceptor.MODID)
+public class ChannelAcceptor
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "channel_acepptor";
+    public static final String MODID = "channel_acceptor";
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public ChannelAcepptor(IEventBus modEventBus, ModContainer modContainer)
+    public ChannelAcceptor(IEventBus modEventBus, ModContainer modContainer)
     {
         // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (ChannelAcepptor) to respond directly to events.
+        // Note that this is necessary if and only if we want *this* class (ChannelAcceptor) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
